5ee10cdac428a2bd411408e6feabe1d4a66f2f72
Vojtěch Jedlička <jedlickav@post.cz>
2025-03-20 23:31:19 +0100

    creation of file for mathlib tests and adding googletests library

---
+++ src/mathlib-test.cpp
@@ -0,0 +1,21 @@
+/****************************************************************** 
+* Project name: IVS 2 project
+* Date: 20.3.2025
+* Last change: 20.3.2024 
+* Author: Vojtěch Jedlička
+* Brief: Test for test driven development of math library 
+*****************************************************************
+/**
+* @file mathlib-test.cpp
+*
+* @brief Test for test driven development of math library
+* @author Vojtěch Jedlička
+*/
+
+#include "gtest/gtest.h"
+#include "mathlib_code.cpp"
+
+using namespace:: testing;
+
+
+/*** End of file mathlib-test.cpp ***/
