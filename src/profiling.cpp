17cfe399ebe26e19f514cfe5536aefa53292f35a
MatarCZ <marek.furis@seznam.cz>
2025-04-30 22:51:59 +0200

    Fixed doxygen comments in profiling.cpp

--- src/profiling.cpp
+++ src/profiling.cpp
@@ -16,13 +16,12 @@
 #include <iostream>
 #include "mathlib-code.h"
 
-/**************************************
+/**
  * @brief Main function, gets input from stdin and computes defined equation and prints result
  * 
  * 
  * @return 0 on success
  */
-
 int main() {
 	float count = 0.0; // Number of elements
 	float number; // Input number
