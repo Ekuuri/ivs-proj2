fff08d41a6f28c8e8c00f4b77595060d241d02f3
xsrajed00 <lorex85@DESKTOP-0B658HH>
2025-04-30 16:30:33 +0200

    Edited onEqualsClicked

--- src/calc.cpp
+++ src/calc.cpp
@@ -200,7 +200,7 @@ void Calculator::onEqualsClicked() {
         }
         else {
             result = QString::number(number, 'f', 4);
-            result = result.replace(QRegularExpression("([\\.][0-9]*?)0+$"), "\\1");
+            result = result.replace(QRegularExpression("([\\.][0-9]*?)0+$"), "\\1"); // Remove unnecessary zeros after decimal point
         }
     }
     ui->Display->setText(result);
